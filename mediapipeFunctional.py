ab798d6015fe7d0e9e872baf2079865f9a4dd3e9
Ben Randoing <benjamin.randoing@duke.edu>
2023-07-05 11:27:09 -0700

    Created simple Flask web app, local host, need metric display

--- mediapipeFunctional.py
+++ mediapipeFunctional.py
@@ -4,16 +4,12 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
-def run():
+def generate_frames():
 	# Initialize MediaPipe Drawing
 	mp_drawing = mp.solutions.drawing_utils
 
 	# Initialize MediaPipe Pose model
 	mp_pose = mp.solutions.pose
-
-	# Curl counter variables
-	counter = 0
-	stage = None
 
 	cap = cv2.VideoCapture(0)
 	## Setup mediapipe instance
@@ -36,6 +32,7 @@ def run():
 			try:
 				landmarks = results.pose_landmarks.landmark
 
+
 				# Get coordinates Lower Body
 				knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
 				       landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
@@ -46,26 +43,21 @@ def run():
 				ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,
 				       landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
 
-
 				# Get coordinates Upper Body
 				shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
 				            landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-				elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
-				         landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-				wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
-				         landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
 
-				# Calculate angle
-				angle = calculate_angle(shoulder, elbow, wrist)
 
+				# Calculate angles
 				angle_trunk = calculate_angle_horz(shoulder, hip)
 
 				angle_shank = calculate_angle_horz(knee, ankle)
 
+
 				# Visualize angle of trunk and shank on video wrt ground
 				cv2.putText(image, str(angle_trunk),
 				            tuple(np.multiply(hip, [640, 480]).astype(int)),
-				            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2,
+				            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2,
 				            cv2.LINE_AA)
 
 				cv2.putText(image, str(angle_shank),
@@ -73,42 +65,8 @@ def run():
 				            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2,
 				            cv2.LINE_AA)
 
-				# Curl counter logic
-				# if angle > 160:
-				# 	stage = "down"
-				# if angle < 30 and stage == 'down':
-				# 	stage = "up"
-				# 	counter += 1
-				# 	print(counter)
-
 			except:
 				pass
-
-			# # Displays landmark names for manipulation
-			# for lndmrk in mp_pose.PoseLandmark:
-			# 	print(lndmrk)
-
-			# # Render curl counter
-			# # Setup status box
-			# cv2.rectangle(image, (0, 0), (225, 73), (245, 117, 16), -1)
-			#
-			# # Rep data
-			# cv2.putText(image, 'REPS', (15, 12),
-			#             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1,
-			#             cv2.LINE_AA)
-			# cv2.putText(image, str(counter),
-			#             (10, 60),
-			#             cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2,
-			#             cv2.LINE_AA)
-			#
-			# # Stage data
-			# cv2.putText(image, 'STAGE', (65, 12),
-			#             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1,
-			#             cv2.LINE_AA)
-			# cv2.putText(image, stage,
-			#             (60, 60),
-			#             cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2,
-			#             cv2.LINE_AA)
 
 			# Render detections
 			mp_drawing.draw_landmarks(image, results.pose_landmarks,
@@ -198,4 +156,4 @@ def calculate_angle_horz(point1, point2):
         return 90.0
 
 if __name__ == '__main__':
-    run()
+    generate_frames()
