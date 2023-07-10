c2bee2e04b251cbced7e6bdf737b51f93982bf9c
Ben Randoing <benjamin.randoing@duke.edu>
2023-07-10 13:19:11 -0700

    Immplementd File based video pose stimates and hip angle plot

--- mediapipeFunctional.py
+++ mediapipeFunctional.py
@@ -3,12 +3,13 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import matplotlib.pyplot as plt
 
 def file_import(path):
 	'''
 	Reads in video file in cv2 capture object
 	'''
-	video_path = "path/to/video/file.mp4"
+	video_path = "IMG_6746.MOV"
 	cap = cv2.VideoCapture(video_path)
 
 	return cap
@@ -26,6 +27,11 @@ def generate_frames_file():
 
 	# Initialize MediaPipe Pose model
 	mp_pose = mp.solutions.pose
+
+	start_frame = 0
+	end_frame = 0
+	threshold_angle_hip = 120
+	hip_angle_data = []
 
 	with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
 		while cap.isOpened():
@@ -45,7 +51,6 @@ def generate_frames_file():
 			try:
 				landmarks = results.pose_landmarks.landmark
 
-
 				# Get coordinates Lower Body
 				knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
 				       landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
@@ -60,22 +65,54 @@ def generate_frames_file():
 				# Calculate angles
 				hip_angle = calculate_angle(knee, hip, shoulder)
 
-				# Visualize angle of trunk and shank on video wrt ground
-				cv2.putText(image, str(angle_trunk),
+				# Update start and end frames
+				if hip_angle < threshold_angle_hip and start_frame == 0:
+					start_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
+				if hip_angle > threshold_angle_hip and start_frame != 0 and end_frame == 0:
+					end_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
+
+
+
+				# Crop and store hip angle data within the squat range
+				if start_frame != 0 and end_frame != 0:
+					hip_angle_data.append(hip_angle)
+
+				hip_angle_data = hip_angle_data[int(start_frame):int(
+					end_frame)]
+
+				# Visualize angle of hip_angle at the hip
+				cv2.putText(image, str(hip_angle),
 				            tuple(np.multiply(hip, [640, 480]).astype(int)),
 				            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2,
-				            cv2.LINE_AA)
-
-				cv2.putText(image, str(angle_shank),
-				            tuple(np.multiply(knee, [640, 480]).astype(int)),
-				            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2,
 				            cv2.LINE_AA)
 
 			except:
 				pass
 
+			# Render detections
+			mp_drawing.draw_landmarks(image, results.pose_landmarks,
+			                          mp_pose.POSE_CONNECTIONS,
+			                          mp_drawing.DrawingSpec(
+				                          color=(245, 117, 66),
+				                          thickness=2,
+				                          circle_radius=2),
+			                          mp_drawing.DrawingSpec(
+				                          color=(245, 66, 230),
+				                          thickness=2,
+				                          circle_radius=2)
+			                          )
 
+			# Display the image
+			cv2.imshow('Squat Video', image)
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
 
+def make_plots(angle_data):
+	plt.figure(1)
+	plt.plot(angle_data)
+	plt.xlabel('Frame')
+	plt.ylabel('Hip Angle (degrees)')
+	plt.title('Hip Angle during Squat')
 
 def generate_frames():
 	# Initialize MediaPipe Drawing
@@ -229,4 +266,4 @@ def calculate_angle_horz(point1, point2):
         return 90.0
 
 if __name__ == '__main__':
-    generate_frames()
+    generate_frames_file()
